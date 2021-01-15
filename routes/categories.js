e948ae18e7551bc1de5e852660f1b81b67649278
Milande Teisserenc bonesteve <milandejf@gmail.com>
2021-01-15 16:24:22 +0100

    add get search

--- routes/categories.js
+++ routes/categories.js
@@ -14,4 +14,16 @@ router.get("/all", function (req, res, next) {
     });
 });
 
+router.get("/search", function (req, res, next) {
+  Category.find({ name: { $regex: req.query.searchText, $options: "i"}}) //.find take in parameters la query. It needs to match with the regex. req.query.q = q= (do the link with q=)
+    .then((respondApi) => {
+      res.status(200).send(respondApi);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
 module.exports = router;
+
