bb9ab47fb9faa2ba022c6ccba5f099b3b4749c65
Menhaj safi <minhaj.sahil01@gmail.com>
2021-01-21 14:41:39 +0100

    deployment

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("index");
 });
+/*if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}*/
 
 module.exports = router;
